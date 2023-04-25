36ed483387df03c284a64ea308309fce4e4875e1
dragonblancorc <123729964+dragonblancorc@users.noreply.github.com>
2023-04-25 14:07:48 +0200

    Update Tacon de oro.sql

--- Tacon de oro.sql
+++ Tacon de oro.sql
@@ -1,9 +1,11 @@
+-- CREACION DE LA BASE DE DATOS
 Drop database if exists tacondeoro;
 create database tacondeoro;
 use tacondeoro;
 
 -- CREACION DE TABLAS
 
+-- CREACION DE LA TABLA 'SOCIO'
 drop table if exists socio;
 create table socio
 (id int not null unique auto_increment,
@@ -11,15 +13,39 @@ nombre varchar(50),
 correoElectronico varchar(100),
 direccion varchar(50),
 poblacion varchar(100),
-primary key (id));
+primary key (id)
+);
 
+-- INSERTAR REGISTROS EN LA TABLA SOCIO
+INSERT INTO `socio`
+(nombre, correoElectronico, direccion, poblacion)
+VALUES
+('Juan', 'juan@asdas.com',  'Avda.A', 'Alcazar'),
+('Laura', 'laura@asdas.com',  'Avda.B', 'Herencia'),
+('Paula', 'paula@asdas.com',  'Avda.C', 'Tomelloso'),
+('Raul', 'raul@asdas.com',  'Avda.D', 'Villafranca')
+;
+
+-- CREACION DE LA TABLA 'TARJETABANCARIA'
 drop table if exists tarjetabancaria;
 create table tarjetabancaria
 (numero varchar(50) not null unique,
 idSocio int,
 primary key (numero),
-foreign key (idSocio) references socio (id));
+foreign key (idSocio) references socio (id)
+);
 
+-- INSERTAR REGISTROS EN LA TABLA TARJETABANCARIA
+INSERT INTO `tarjetabancaria`
+(numero, idSocio)
+VALUES
+('742572', 1),
+('763445', 1),
+('625145', 2),
+('4214125', 3)
+;
+
+-- CREACION DE LA TABLA 'EMPRESATRANSPORTE'
 drop table if exists empresatransporte;
 create table empresatransporte
 (nombre varchar(100) not null unique,
@@ -28,6 +54,16 @@ domicilioFiscal varchar(100),
 primary key (nombre)
 );
 
+-- INSERTAR REGISTROS EN LA TABLA EMPRESATRANSPORTE
+INSERT INTO `empresatransporte`
+(nombre, CIF, domicilioFiscal)
+VALUES
+('Empresa1', '13453245', 'Bahamas'),
+('Empresa2', '62354532', 'Irlanda'),
+('Empresa3', '12351123', 'Bahamas')
+;
+
+-- CREACION DE LA TABLA 'RUTA'
 drop table if exists ruta;
 create table ruta
 (id int not null unique auto_increment,
@@ -38,6 +74,16 @@ primary key (id),
 foreign key (empresaT) references empresatransporte (nombre)
 );
 
+-- INSERTAR REGISTROS EN LA TABLA RUTA
+INSERT INTO `ruta`
+(areas, diareparto, empresaT)
+VALUES
+('Area1', 'Lunes', 'Empresa1'),
+('Area2', 'Martes', 'Empresa2'),
+('Area3', 'Viernes', 'Empresa3')
+;
+
+-- CREACION DE LA TABLA 'PEDIDOS'
 drop table if exists pedidos;
 create table pedidos
 (codigo int not null unique auto_increment,
@@ -50,6 +96,7 @@ foreign key(idsocio) references socio (id),
 foreign key (idRuta) references ruta (id)
 );
 
+-- CREACION DE LA TABLA 'ARTICULO'
 drop table if exists articulo;
 create table articulo
 (id int not null unique auto_increment,
@@ -69,32 +116,26 @@ tallaComplemento int,
 primary key (id)
 );
 
-drop table if exists lineaspedido;
-create table lineaspedido
-(codPedido int not null,
-idArticulo int not null,
-cantidad int,
-primary key (codPedido, idArticulo),
-unique (codPedido, idArticulo),
-foreign key (codPedido) references pedidos (codigo),
-foreign key (idArticulo) references articulo (id) 
-);
-
--- INSERTAR REGISTROS
-
--- Zapatos
+-- INSERTAR REGISTROS EN LA TABLA ARTICULO (ZAPATOS)
 INSERT INTO `articulo`
 (nombre, tipoArticulo, precio, descripcion, material, stock, foto, numeroZapato, tipoZapato)
 VALUES
-('Z1', 'Zapato',  80, 'Modelo original de acabados impecables y la cantidad perfecta de reflectante', 'Sintetico', 20, 'Z1.png', 38, 'Zapatilla'),
-('Z2', 'Zapato',  90, 'Modelo que ofrece una estabilidad premium y una amortiguación cómoda', 'Ante', 15, 'Z2.png', 39, 'Zapato'),
-('Z3', 'Zapato',  100, 'Modelo creado para la cancha y adaptado al estilo urbano', 'Goma', 30, 'Z3.png', 36, 'Chancla')
--- ('Nike Air Force 1', 'Zapato',  80, 'Modelo original de baloncesto que introduce costuras duraderas, acabados impecables y la cantidad perfecta de reflectante', 'Sintetico', 20, 'NikeAirForce1.png', 38, 'Zapatilla'),
--- ('Nike Air Max Plus', 'Zapato',  90, 'Modelo Tuned Air que ofrece una estabilidad premium y una amortiguación cómoda', 'Sintetico', 15, 'NikeAirMaxPlus.png', 39, 'Zapatilla'),
--- ('Nike Dunk Low', 'Zapato',  100, 'Modelo creado para la cancha y adaptado al estilo urbano, con revestimientos de brillo perfecto y colores clásicos ', 'Sintetico', 30, 'NikeDunkLow.png', 36, 'Zapatilla')
+-- ('Z1', 'Zapato',  80, 'Modelo original de acabados impecables y la cantidad perfecta de reflectante', 'Sintetico', 20, 'Z1.png', 38, 'Zapatilla'),
+-- ('Z2', 'Zapato',  90, 'Modelo que ofrece una estabilidad premium y una amortiguación cómoda', 'Ante', 15, 'Z2.png', 39, 'Zapato'),
+-- ('Z3', 'Zapato',  100, 'Modelo creado para la cancha y adaptado al estilo urbano', 'Goma', 30, 'Z3.png', 36, 'Chancla')
+('Nike Air Force 1', 'Zapato',  80, 'Modelo original de acabados impecables y la cantidad perfecta de reflectante', 'Sintetico', 10, 'NikeAirForce1.png', 38, 'Zapatilla'),
+('Nike Air Force 1', 'Zapato',  80, 'Modelo original de acabados impecables y la cantidad perfecta de reflectante', 'Sintetico', 5, 'NikeAirForce1.png', 39, 'Zapatilla'),
+('Nike Air Force 1', 'Zapato',  80, 'Modelo original de acabados impecables y la cantidad perfecta de reflectante', 'Sintetico', 20, 'NikeAirForce1.png', 40, 'Zapatilla'),
+('Nike Air Force 1', 'Zapato',  80, 'Modelo original de acabados impecables y la cantidad perfecta de reflectante', 'Sintetico', 25, 'NikeAirForce1.png', 41, 'Zapatilla'),
+('Nike Air Max Plus', 'Zapato',  90, 'Modelo que ofrece una estabilidad premium y una amortiguación cómoda', 'Sintetico', 40, 'NikeAirMaxPlus.png', 39, 'Zapatilla'),
+('Nike Air Max Plus', 'Zapato',  90, 'Modelo que ofrece una estabilidad premium y una amortiguación cómoda', 'Sintetico', 50, 'NikeAirMaxPlus.png', 40, 'Zapatilla'),
+('Nike Air Max Plus', 'Zapato',  90, 'Modelo que ofrece una estabilidad premium y una amortiguación cómoda', 'Sintetico', 60, 'NikeAirMaxPlus.png', 41, 'Zapatilla'),
+('Nike Air Max Plus', 'Zapato',  90, 'Modelo que ofrece una estabilidad premium y una amortiguación cómoda', 'Sintetico', 70, 'NikeAirMaxPlus.png', 42, 'Zapatilla'),
+('Nike Air Max Plus', 'Zapato',  90, 'Modelo que ofrece una estabilidad premium y una amortiguación cómoda', 'Sintetico', 80, 'NikeAirMaxPlus.png', 43, 'Zapatilla'),
+('Nike Dunk Low', 'Zapato',  100, 'Modelo creado para la cancha y adaptado al estilo urbano', 'Sintetico', 30, 'NikeDunkLow.png', 36, 'Zapatilla')
 ;
 
--- Bolsos
+-- INSERTAR REGISTROS EN LA TABLA ARTICULO (BOLSOS)
 INSERT INTO `articulo`
 (nombre, tipoArticulo, precio, descripcion, material, stock, foto, tipoBolso)
 VALUES
@@ -110,25 +151,21 @@ VALUES
 ('C1', 'Complemento',  25, 'Cinturon1', 'Sintetico', 80, 'C1.png', 'Cinturón', 85),
 ('C2', 'Complemento',  25, 'Cinturon1', 'Sintetico', 80, 'C1.png', 'Cinturón', 90),
 ('C3', 'Complemento',  15, 'Guantes1', 'Lana', 70, 'C2.png', 'Guantes', 6),
-('C4', 'Complemento',  10, 'Guantes2 manoplas', 'Lana', 60, 'C3.png', 'Guantes', 7)
+('C4', 'Complemento',  10, 'Guantes2 manoplas', 'Lana', 60, 'C3.png', 'Guantes', 7),
+('C1', 'Complemento',  25, 'Cinturon1', 'Sintetico', 70, 'C1.png', 'Cinturón', 75),
+('C2', 'Complemento',  27, 'Cinturon1', 'Sintetico', 85, 'C1.png', 'Cinturón', 95),
+('C3', 'Complemento',  15, 'Guantes1', 'Latex', 70, 'C2.png', 'Guantes', 6),
+('C4', 'Complemento',  10, 'Guantes2 manoplas', 'Lana', 60, 'C3.png', 'Guantes', 9)
 ;
 
--- TarjetasBancarias
-INSERT INTO `tarjetabancaria`
-(numero, idSocio)
-VALUES
-('155146824674', '1'),
-('513546519814', '2'),
-('541656518465', '3'),
-('651986598518', '4')
-;
-
--- Empresatransporte
-INSERT INTO `empresatransporte`
-(nombre, CIF, domicilioFiscal)
-VALUES
-('puntapie', '065415826T', 'Calle Las Cruces Nº5'),
-('yoyo', '135695169U', 'Avinida La Nueva Nº38'),
-('amigofiesta', '385732245Y', 'Calle Los Catolicos Nº23')
-;
-
+-- CREACION DE LA TABLA 'LINEASPEDIDO'
+drop table if exists lineaspedido;
+create table lineaspedido
+(codPedido int not null,
+idArticulo int not null,
+cantidad int,
+primary key (codPedido, idArticulo),
+unique (codPedido, idArticulo),
+foreign key (codPedido) references pedidos (codigo),
+foreign key (idArticulo) references articulo (id) 
+);
